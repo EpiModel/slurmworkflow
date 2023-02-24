37da4840cf91cdf21daa98c6dc5b4f9e6e94adf6
Adrien Le Guillou <git@aleguillou.org>
2023-02-24 10:08:42 +0100

    Add time calculations to step templates
    fixes #21

--- inst/templates/step_tmpl_map.R
+++ inst/templates/step_tmpl_map.R
@@ -1,3 +1,10 @@
+# Time Calulation --------------------------------------------------------------
+print("Workflow step starting at:")
+swf__start_time <- Sys.time()
+print(swf__start_time)
+print("          ##########          ")
+
+# Step Template ----------------------------------------------------------------
 step_dir <- Sys.getenv("SWF__CUR_DIR")
 swf__tmpl_elts <- readRDS(fs::path(step_dir, "map.rds"))
 rm(step_dir)
@@ -43,3 +50,8 @@ do.call(
   what = swf__tmpl_elts[["FUN"]],
   args = swf__tmpl_elts[["args"]]
 )
+
+# Time Calulation --------------------------------------------------------------
+print("          ##########          ")
+print("Workflow ran for:")
+print(Sys.time() - swf__start_time)
